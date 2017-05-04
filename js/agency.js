ef4a9de892364caf3a8c886256c92b28b2276b77
Lucas FOUSSIER <lucasfoussier@gmail.com>
2017-05-04 17:54:21 +0200

    1

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,55 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+// .modal-backdrop classes
+
+
+
+var trigered = 1; // pour désactiver les popups, passer a 1
+var popupdelay = 9; //délai en secondes avant l'apparition du popup
+
+var start = new Date().getTime();
+
+$(document).scroll(popup)
+
+if (getCookie(11832) == 1){
+trigered = 1;
+}
+else {
+trigered = 0;
+}
+
+function popup() {
+
+      if ((new Date().getTime() - start) > popupdelay * 1000){
+         if (trigered == 0){
+           setCookie(11832, 1, 29)
+         $('#news').modal('show');
+         trigered = 1;
+         }
+      }
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
